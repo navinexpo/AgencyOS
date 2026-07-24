9eeb79ee3debea687de72067e64122a9ccb0e03c
navinexpo <navinexpo2020@gmail.com>
2026-07-24 23:21:36 +0530

    Home Page Component

---
+++ Downloads/AgencyOs-main (2)/AgencyOs-main/frontend/src/components/Layout.jsx
@@ -0,0 +1,62 @@
+import {Outlet, Link} from "react-router-dom"
+import {SignedIn, SignedOut, UserButton, OrganizationSwitcher, useOrganization} from "@clerk/clerk-react";
+
+function Layout() {
+    const Organization = useOrganization()
+
+    return <div className={"layout"}>
+        <div className={"nav"}>
+            <div className={"nav-container"}>
+                <Link to={"/"} className={"nav-logo"}>
+                     AgencyOS
+                </Link>
+
+                <div className={"nav-links"}>
+                    <Link to={"/pricing"} className={"nav-link"}>
+                        Pricing
+                    </Link>
+
+                    <SignedOut>
+                    <Link to={"/sign-in"} className={"nav-link"}>
+                        Sign In
+                    </Link>
+                    <Link to={"/sign-up"} className={"btn btn-primary"}>
+                        Sign Up
+                    </Link>
+                    </SignedOut>
+
+                <SignedIn>
+                    <OrganizationSwitcher
+                        hidePersonal
+                        afterCreateOrganizationUrl={"dashboard"}
+                        afterSelectOrganizationUrl={"dashboard"}
+                        createOrganizationMode={"modal"}
+                        appearance={{
+                            elements: {
+                                userPreviewMainIdentifierText__personalWorkspace: {color: "white"},
+                                organizationPreviewMainIdentifier__organizationSwitcherTrigger: {color: "white"}
+                            }
+                        }}
+                    />
+                    {Organization && 
+                    <Link to={"/dashboard"} className={"nav-link"}>
+                            Dashboard
+                    </Link>}
+
+                    <UserButton />
+                </SignedIn>
+                </div>
+            </div>
+        </div>
+
+        <main>
+            <Outlet />
+        </main>
+
+
+    </div>
+
+} 
+
+
+export default Layout
