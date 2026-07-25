d7b3e7bda920e74c76c2da6a42319bdbde1831c9
navinexpo <navinexpo2020@gmail.com>
2026-07-25 23:10:46 +0530

    SigninPage component is used to render

--- Downloads/AgencyOs-main (2)/AgencyOs-main/frontend/src/pages/SigninPage.jsx
+++ Downloads/AgencyOs-main (2)/AgencyOs-main/frontend/src/pages/SigninPage.jsx
@@ -1,5 +1,5 @@
 import {SignIn, SignUp} from "@clerk/clerk-react"
-
+// SigninPage component is used to render the sign-in page of the application. It uses the SignIn component from Clerk to handle user authentication. The routing prop is set to "path" to enable path-based routing, and the path prop is set to "/sign-in" to specify the URL for the sign-in page. The signUpUrl prop is set to "/sign-up" to provide a link for users who want to sign up instead of signing in.
 function SigninPage()
 {
     return <div className={"auth-container"}>
