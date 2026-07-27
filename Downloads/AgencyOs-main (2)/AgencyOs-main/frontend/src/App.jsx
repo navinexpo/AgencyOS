8d27cc7548504755d1dfdc25df7fb4865698c6a6
navinexpo <navinexpo2020@gmail.com>
2026-07-27 15:21:13 +0530

    ProtectedRoute component is a wrapper

--- Downloads/AgencyOs-main (2)/AgencyOs-main/frontend/src/App.jsx
+++ Downloads/AgencyOs-main (2)/AgencyOs-main/frontend/src/App.jsx
@@ -6,7 +6,7 @@ import SigninPage from "./pages/SigninPage.jsx"
 import SignupPage from "./pages/SignupPage.jsx"
 import DashboardPage from "./pages/DashboardPage.jsx"
 import PricingPage from "./pages/PricingPage.jsx"
-
+// The ProtectedRoute component is a wrapper that checks if the user is signed in. If the user is signed in, it renders the children components. If the user is signed out, it redirects them to the sign-in page using the RedirectToSignIn component from Clerk.
 function ProtectedRoute({children}) {
   return<>
     <SignedIn>{children}</SignedIn>
