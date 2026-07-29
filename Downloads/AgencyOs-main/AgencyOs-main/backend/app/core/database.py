dff94206df9b30172fc019e133a196217ab7575c
navinexpo <navinexpo2020@gmail.com>
2026-07-29 20:45:57 +0530

    configured Session class

--- Downloads/AgencyOs-main/AgencyOs-main/backend/app/core/database.py
+++ Downloads/AgencyOs-main/AgencyOs-main/backend/app/core/database.py
@@ -6,7 +6,7 @@ engine = create_engine(
     settings.DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
-
+# Create a configured "Session" class
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
